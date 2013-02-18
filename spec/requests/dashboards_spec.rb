cbcc526082fea11e19d61196569b2ff4e9f9595c
Alexander Glushkov <cutalion@gmail.com>
2013-02-18 23:10:21 +0200

    Test dashboard is ok

---
+++ spec/requests/dashboards_spec.rb
@@ -0,0 +1,10 @@
+require 'spec_helper'
+
+describe "Dashboards" do
+  describe "GET /dashboards" do
+    it "works! (now write some real specs)" do
+      get root_path
+      response.status.should be(200)
+    end
+  end
+end
