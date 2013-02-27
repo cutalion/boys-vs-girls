daa6fecce646fe2b723efa9df34afd57dfc82fed
Andrey Kekuh <a.kekuh@gmail.com>
2013-02-27 18:52:07 +0200

    new test

--- test/unit/message_test.rb
+++ test/unit/message_test.rb
@@ -3,11 +3,16 @@ require 'test_helper'
 class MessageTest < ActiveSupport::TestCase
   test "Should_not_save_message_without_user_id" do 
 		message = Message.new
-		assert !message.save
+		message.valid? # => false
+		message.errors[:user_id]
+ 		assert message.errors[:user_id].include? "can't be blank"
   end
   
+
   test "Should_not_save_message_without_text" do 
 		message = Message.new
-		assert !message.save
-	end
+		message.valid? # => false
+		message.errors[:text]
+ 		assert message.errors[:text].include? "can't be blank"
+  end
 end
