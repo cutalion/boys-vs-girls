053c0ae574d3afb509e39444993d2cb7645850d5
Alexander Glushkov <cutalion@gmail.com>
2013-02-27 19:24:06 +0200

    Move tests to spec folder

--- spec/models/message_spec.rb
+++ spec/models/message_spec.rb
@@ -1,4 +1,17 @@
 require 'spec_helper'
 
 describe Message do
+  it "should validate presence of user id" do
+    message = Message.new
+    message.valid? # => false
+    message.errors[:user_id]
+    message.errors[:user_id].should include "can't be blank"
+  end
+
+  it "should validate presence of text" do
+    message = Message.new
+    message.valid? # => false
+    message.errors[:text]
+    message.errors[:text].should include "can't be blank"
+  end
 end
