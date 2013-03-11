eac1d0deb0409e308a1430bacb259786be84b146
Kekuh <a.kekuh@gmail.com>
2013-03-11 18:59:37 +0200

    add show

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,9 @@
 class DashboardController < ApplicationController
   def show
+    @messages = Message.all
+
+    respond_to do |format|
+      format.html # dashboard.html.erb
+      format.json { render json: @message }
   end
 end
