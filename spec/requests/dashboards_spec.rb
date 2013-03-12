ee17ffd1f263e7d97fa8e69d97551212f1b9d5ee
Alexander Glushkov <cutalion@gmail.com>
2013-03-12 16:43:26 +0200

    Integration test for posting a message

--- spec/requests/dashboards_spec.rb
+++ spec/requests/dashboards_spec.rb
@@ -7,4 +7,23 @@ describe "Dashboards" do
       response.status.should be(200)
     end
   end
+
+  describe "post a message", type: :feature do
+    it "should add new message" do
+      signin
+      visit "/"
+      fill_in "Text", with: "2000"
+      click_button "Save"
+      page.should have_content "Message was successfully created"
+      Message.last.text.should == "2000"
+    end
+  end
+
+  def signin
+    User.create! email: "email@example.com", password: "password", password_confirmation: "password"
+    visit new_user_session_path
+    fill_in "Email", with: "email@example.com"
+    fill_in "Password", with: "password"
+    click_button "Sign in"
+  end
 end
